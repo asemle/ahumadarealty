b58cb600349f66fb257de1d852891215899cc8af
asemle <asemle@umass.edu>
2017-02-27 13:45:55 -0500

    haven't commited in a while

---
+++ app.js
@@ -0,0 +1,16 @@
+angular.module("app", ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: "views/home.html",
+      controller: 'ctrl'
+    })
+    .state('listings', {
+      url: '/listings',
+      templateUrl: "views/listings.html",
+      controller: 'ctrl'
+    })
+    $urlRouterProvider
+           .otherwise('/');
+});
---
+++ ctrl.js
@@ -0,0 +1,9 @@
+angular.module("app")
+.controller("ctrl", function($state) {
+
+  // console.log($state)
+  // if($state.current.name === "home") {
+  //
+  // }
+
+})
